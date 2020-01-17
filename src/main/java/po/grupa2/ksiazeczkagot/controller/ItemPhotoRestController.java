dc418160f43bed7170abf11b59287dfd679f20be
kamil <kamillo_prz@gazeta.pl>
2020-01-17 13:37:05 +0100

    Add CORS policy

--- src/main/java/po/grupa2/ksiazeczkagot/controller/ItemPhotoRestController.java
+++ src/main/java/po/grupa2/ksiazeczkagot/controller/ItemPhotoRestController.java
@@ -6,12 +6,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "${cors.origin.angularapp}", allowedHeaders = "*")
 @RequestMapping(RestMappings.ITEM_PHOTO)
 public class ItemPhotoRestController {
   @GetMapping(RestMappings.FILE_NAME)
