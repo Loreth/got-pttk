ad7c77e07fc618182ac9da0a587587b5731581e0
Kamil Przeniosło <kamil.przenioslo@gazeta.pl>
2020-01-18 16:14:54 +0100

    Add filterable search for OdcinkiPunktowane

---
+++ src/main/java/po/grupa2/ksiazeczkagot/specifications/OdcinekPunktowanySpecification.java
@@ -0,0 +1,24 @@
+package po.grupa2.ksiazeczkagot.specifications;
+
+import net.kaczmarzyk.spring.data.jpa.domain.EqualIgnoreCase;
+import net.kaczmarzyk.spring.data.jpa.domain.GreaterThanOrEqual;
+import net.kaczmarzyk.spring.data.jpa.domain.In;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.JoinFetch;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.Joins;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
+import org.springframework.data.jpa.domain.Specification;
+import po.grupa2.ksiazeczkagot.persistance.entities.OdcinekPunktowanyEntity;
+
+@Joins(fetch = {@JoinFetch(paths = "punktPoczatkowy"), @JoinFetch(paths = "punktKoncowy")})
+@And({
+  @Spec(path = "punktacja", params = "punktacjaMin", spec = GreaterThanOrEqual.class),
+  @Spec(path = "punktPoczatkowy.nazwa", params = "punktPoczatkowy", spec = EqualIgnoreCase.class),
+  @Spec(path = "punktKoncowy.nazwa", params = "punktKoncowy", spec = EqualIgnoreCase.class),
+  @Spec(
+      path = "punktPoczatkowy.terenGorski.grupaGorska.nazwa",
+      params = "grupaGorska",
+      spec = EqualIgnoreCase.class),
+  @Spec(path = "punktPoczatkowy.terenGorski.nazwa", params = "terenGorski", spec = In.class),
+})
+public interface OdcinekPunktowanySpecification extends Specification<OdcinekPunktowanyEntity> {}
